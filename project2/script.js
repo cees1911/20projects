540c8758caf2a95df36399841c03ebe4d9e168f4
Cees Klap <ceesklap@gmail.com>
2020-06-21 16:42:23 +0200

    second part js done

--- project2/script.js
+++ project2/script.js
@@ -5,8 +5,19 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice){
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+};
+
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    const seatsIndex = [...selectedSeats].map(seat =>{
+      return[...seats].indexOf(seat)
+    })
+    
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
     
     const selectedSeatsCount = selectedSeats.length;
 
@@ -16,16 +27,15 @@ function updateSelectedCount(){
 
 movieSelect.addEventListener("change", event =>{
     ticketPrice = +event.target.value;
+    setMovieData(event.target.selectedIndex, event.target.value)
     updateSelectedCount();
 });
 
 container.addEventListener("click", (event) => {
-  if (
-    event.target.classList.contains("seat") &&
-    !event.target.classList.contains("occupied")
-  ) {
+  if (event.target.classList.contains("seat") &&
+    !event.target.classList.contains("occupied")) 
+    {
     event.target.classList.toggle("selected");
-
     updateSelectedCount();
   }
 });
